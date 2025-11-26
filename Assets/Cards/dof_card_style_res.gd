d789c57a803be34f4f69ced1cab42aadc1595d54
Belly <dbelfrageyoung@gmail.com>
2025-11-26 14:31:59 +1100

    added Dragon card
    made a dragon card resource

--- Assets/Cards/dof_card_style_res.gd
+++ Assets/Cards/dof_card_style_res.gd
@@ -1,21 +1,9 @@
 class_name DofCardStyleResource extends CardResource
 #this defines card values
-@export var name: String
+@export var card_name: String
+@export var strength: int
 @export var top_texture: Texture2D
 #@export var current_modiffier: modiffier = modiffier.NONE
 #@export var card_suit: suit = suit.ALL
 #@export var value: int = 1
 #
-#enum modiffier {
-	#NONE,
-	#GOLD,
-	#STEEL,
-#}
-#
-#enum suit{
-	#CLUBS,
-	#DIAMOND,
-	#HEART,
-	#SPADE,
-	#ALL,
-#}
--- card_layouts/DoF_style.gd
+++ card_layouts/DoF_style.gd
@@ -1,8 +1,9 @@
 extends CardLayout
 #this applies values to a card
-@onready var card_color: PanelContainer = %CardColor
+@onready var card_color: PanelContainer = %PanelContainer
 @onready var texture_rect: TextureRect = %TextureRect
-@onready var value_label: Label = %ValueLabel
+@onready var name_label: Label = %Label
+@onready var strength_label: Label = %Label2
 
 var res: DofCardStyleResource
 
@@ -10,30 +11,5 @@ func _update_display() -> void:
 	res = card_resource as DofCardStyleResource
 	#set_color()
 	texture_rect.texture = res.top_texture
-	#set_value()
-
-func set_color():
-	match res.current_modiffier:
-		res.modiffier.NONE:
-			card_color.self_modulate = Color.BISQUE
-		res.modiffier.GOLD:
-			card_color.self_modulate = Color.GOLD
-		res.modiffier.STEEL:
-			card_color.self_modulate = Color.LIGHT_STEEL_BLUE
-
-func set_value():
-	var text: String = ""
-	
-	match res.value:
-		1:
-			text = "A"
-		11:
-			text = "J"
-		12:
-			text = "Q"
-		13:
-			text = "K"
-		_:
-			text = str(res.value)
-	
-	value_label.text = text
+	name_label.text = res.card_name
+	strength_label.text = str(res.strength)
