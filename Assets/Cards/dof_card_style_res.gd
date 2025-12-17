d9e5227c136b3f65a717627685c4c337c2671af9
Sean O'Connell <pinky@paperticketstudios.com>
2025-12-17 13:08:04 +1100

    Added system for waiting for resolution to complete

--- Assets/Cards/dof_card_style_res.gd
+++ Assets/Cards/dof_card_style_res.gd
@@ -9,16 +9,24 @@ enum CardType {Warrior, Rogue, Mystic, Monster, Jester}
 @export var top_texture: Texture2D
 
 func on_leader_reveal() -> void:
-	print("[DoFCSR(",card_name,")] OnLeaderReveal.")
+	print("[DoFCSR(",card_name,")] OnLeaderReveal finished, sending completed resolution signal...")
+	await DeckOfFate.instance.get_tree().process_frame
+	DeckOfFate.complete_resolution()
 	
 func on_support_reveal() -> void:
-	print("[DoFCSR(",card_name,")] OnSupportReveal.")
+	print("[DoFCSR(",card_name,")] OnSupportReveal finished, sending completed resolution signal...")
+	await DeckOfFate.instance.get_tree().process_frame
+	DeckOfFate.complete_resolution()
 
 func on_combat_finished() -> void:
-	print("[DoFCSR(",card_name,")] OnCombatFinished.")
+	print("[DoFCSR(",card_name,")] OnCombatFinished finished, sending completed resolution signal...")
+	await DeckOfFate.instance.get_tree().process_frame
+	DeckOfFate.complete_resolution()
 
 func on_enter_backline() -> void:
-	print("[DoFCSR(",card_name,")] OnEnterBackline.")
+	print("[DoFCSR(",card_name,")] OnEnterBackline finished, sending completed resolution signal...")
+	await DeckOfFate.instance.get_tree().process_frame
+	DeckOfFate.complete_resolution()
 
 func calculate_adjacency(card:DofCardStyleResource) -> bool:
 	print("[DoFCSR(",card_name,")] CalculateAdjacency using '",card,"'")
